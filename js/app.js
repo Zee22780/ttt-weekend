5d46260db3e43a65bb8aa659f8d455e1426be0cf
zuri227 <zuri22780@gmail.com>
2022-02-05 14:35:38 -0500

    Create cached elements

--- js/app.js
+++ js/app.js
@@ -3,11 +3,20 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let squares, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const sqZero = document.getElementById("sq0")
+const sqOne = document.getElementById("sq1")
+const sqTwo = document.getElementById("sq2")
+const sqThree = document.getElementById("sq3")
+const sqFour = document.getElementById("sq4")
+const sqFive = document.getElementById("sq5")
+const sqSix = document.getElementById("sq6")
+const sqSeven = document.getElementById("sq7")
+const sqEight = document.getElementById("sq8")
+const gameStat
 
 
 /*----------------------------- Event Listeners -----------------------------*/
