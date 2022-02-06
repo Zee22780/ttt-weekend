1e7e88ef08075319dfde371ba4a8b36a9ae2cd78
zuri227 <zuri22780@gmail.com>
2022-02-06 13:49:02 -0500

    Work on render function

--- js/app.js
+++ js/app.js
@@ -6,41 +6,37 @@ const winningCombos = [
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board, turn, winner, playerX, playerO, 
+let board, squares, turn, winner, playerX, playerO
 
 
 /*------------------------ Cached Element References ------------------------*/
-const sqZero = document.getElementById("sq0")
-const sqOne = document.getElementById("sq1")
-const sqTwo = document.getElementById("sq2")
-const sqThree = document.getElementById("sq3")
-const sqFour = document.getElementById("sq4")
-const sqFive = document.getElementById("sq5")
-const sqSix = document.getElementById("sq6")
-const sqSeven = document.getElementById("sq7")
-const sqEight = document.getElementById("sq8")
-const gameStat = null;
-let displayMsg = document.getElementById("h2")
+const squarePlace = document.querySelectorAll("square")
+const gameStat = null
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
-init() {
+init() 
+
+function init() {
   // 3.2.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square. Index 0 represents the top-left square. Index 1 represents the top-middle square. So on, continuing through the entire board until Index 8 maps to the bottom-right square.
-  let board = [null, null, null, null, null, null, null, null]
+  board = [null, null, null, null, null, null, null, null]
   // 3.2.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1. Initialize the winner variable to null. This represents that there is no winner or tie yet. The winner variable will hold the player value (1 or -1) if there's a winner. The winner will hold a 'T' if there's a tie.
-  let playerX = 1
-  let playerO = -1 
-  let winner = null
+  playerX = 1
+  playerO = -1 
+  winner = null
 
-  displayMsg.textContent = "Welcome, It's Your Turn! Click a Box!"
-  console.log(displayMsg)
 }
+// render() 
 
-render() {
-  let board = 
-}
+// 3.3.1) Loop over the board array (which represents the squares on the page), and for each iteration: 3.3.1.1) Use the index of the iteration to access the square in the squares array that corresponds with the current cell being iterated over in the board array
+squares = []
 
-getWinner ()
+board.forEach(function(){
+  
+})
+
+
+// getWinner ()
